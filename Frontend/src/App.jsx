da1fec0932854a04588acdfe24b6e89228ade26c
Kashif Ali <bhuttokashifali957@gmail.com>
2025-04-15 09:28:33 +0500

    routes configured

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-function App() {
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/login";
+import Landing from "./Pages/LandingPage";
+import Courses from "./Pages/Courses";
+const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+      </Routes>
     </>
   );
-}
-
+};
 export default App;
